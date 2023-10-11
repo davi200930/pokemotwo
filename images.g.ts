a492570e0a0e0cccda1265ee17aa1248f6856912
davi200930 <147517381+davi200930@users.noreply.github.com>
2023-10-11 18:55:45 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "teleportation":return img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+6 6 6 6 6 6 6 6 e e e e e e e e 
+6 6 6 6 6 6 6 6 e e e e e e e e 
+6 6 6 6 6 6 6 6 e e e e e e e e 
+6 6 6 6 6 6 6 6 e e e e e e e e 
+8 8 8 8 c c c c e e e e 4 4 4 4 
+8 8 8 8 c c c c e e e e 4 4 4 4 
+8 8 8 8 c c c c e e e e 4 4 4 4 
+8 8 8 8 c c c c e e e e 4 4 4 4 
+8 8 8 8 6 6 6 6 e e e e 4 4 4 4 
+8 8 8 8 6 6 6 6 e e e e 4 4 4 4 
+8 8 8 8 6 6 6 6 e e e e 4 4 4 4 
+8 8 8 8 6 6 6 6 e e e e 4 4 4 4 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
